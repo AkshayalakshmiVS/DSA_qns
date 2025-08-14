26ad7ca163624caa599d76b46640a88bdd01d91b
Akshaya Lakshmi V S <128115963+AkshayalakshmiVS@users.noreply.github.com>
2025-08-14 20:45:44 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -161,5 +161,160 @@ output:4
   ****
 
 PATTERN9:
-  
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+output:5
+	* 
+	* *
+	* * *
+	* * * *
+	* * * * *
+	* * * *
+	* * * 
+	* * 
+	* 
+PATTERN10:
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+output:4
+   *
+  * * *
+ * * * * * 
+* * * * * * *
+PATTERN11:
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=i-1;j++){
+		        System.out.print(" ");
+		    }
+		    for(int k=1;k<=2*n-(2*i-1);k++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
+output:4
+*******
+ *****
+  ***
+   *
+PATTERN12:
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=n-i;j++){
+		        System.out.print(" ");
+		    }
+		    for(int k=1;k<=2*i-1;k++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+		for(int i=1;i<=n-1;i++){
+		    for(int j=1;j<=i;j++){
+		        System.out.print(" ");
+		    }
+		    for(int k=1;k<=2*n-(2*i+1);k++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
+output:4
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+PATTERN13:
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int count=1;
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=i;j++){
+		        System.out.print(count+" ");
+		        count++;
+		    }
+		    System.out.println();
+		}
+	}
+}
+output:4
+1
+2 3 
+4 5 6
+7 8 9 10
+PATTERN14:
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=i;j++){
+		        System.out.print(i);
+		      
+		    }
+		    System.out.println();
+		}
+	}
+}
+output:6
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+6 6 6 6 6 6 
+
+
+	
 
