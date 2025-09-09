5de206713aabbec17987c618cd11d21b17d30887
Akshaya Lakshmi V S <128115963+AkshayalakshmiVS@users.noreply.github.com>
2025-09-09 22:58:13 +0530

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -58,3 +58,42 @@ public class Main{
         
     }
 }
+QN4:Write a program to print the divisors of a number recursively.
+import java.util.*;
+public class Main{
+    public void printdiv(int n,int i){
+        if(i>n){
+            return ;
+        }
+        if(n%i==0){
+            System.out.print(i+" ");
+        }
+        printdiv(n,i+1);
+    }
+    public static void main(String[] args){
+        Main dig=new Main();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        dig.printdiv(n,1);
+        
+    }
+}
+QN5:Write a Program to find the power of a number recursively. Take two inputs, number and the power.
+import java.util.*;
+public class Main{
+    int pow(int n1,int n2){
+        int res=1;
+        if(n2==0){
+            return 1;
+        }
+        return n1*pow(n1,n2-1);
+    }
+    public static void main(String[] args){
+        Main dig=new Main();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n2=sc.nextInt();
+        System.out.print(dig.pow(n,n2));
+        
+    }
+}
